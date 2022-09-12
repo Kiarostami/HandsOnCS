3a3508e15b209220e7e246a7ba457165743d5062
Sina Kiarostami <48089001+Kiarostami@users.noreply.github.com>
2022-09-12 18:03:40 +0300

    Create DefangIPV4.cs

---
+++ DefangIPV4.cs
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public string DefangIPaddr(string address) {
+    
+        //string ad = address;
+        //ad = ad.Replace(".", "[.]");
+        return address.Replace(".", "[.]");
+        
+    }
+    
+}
