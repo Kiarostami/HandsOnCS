84615c150c4b31930d42fa40505adfa1d45d9867
Sina Kiarostami <48089001+Kiarostami@users.noreply.github.com>
2022-09-19 18:51:04 +0300

    Create GoalParser.cs

---
+++ GoalParser.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public string Interpret(string command) {
+        command = command.Replace("()", "o");
+        command = command.Replace("(al)", "al");
+        return command;
+    }
+}
