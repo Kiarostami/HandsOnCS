e90b76196c0dee59c81486ff5cef020fdd442eb3
Sina Kiarostami <48089001+Kiarostami@users.noreply.github.com>
2022-09-21 15:26:20 +0300

    Create ShuffleArray.cs

---
+++ ShuffleArray.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] Shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.Length];
+        for(int i = 0; i < n; i++)
+        {
+            ans[2*i] = nums[i];
+            ans[2*i+1] = nums[n+i];   
+        }
+        return ans;
+    }
+}
